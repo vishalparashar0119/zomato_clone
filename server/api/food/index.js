4d23babbb3241c061ed1853952980c2f19ae99e5
dont touch my mac <vishal@vishals-MacBook-Pro.local>
2023-06-11 23:07:58 +0530

    add some new api routs in resturant

--- server/api/food/index.js
+++ server/api/food/index.js
@@ -1,4 +1,4 @@
-import express  from "express";
+import express, { response }  from "express";
 
 import {FoodModel, UserModel} from "../../database/allModel"
 
@@ -42,7 +42,7 @@ Router.get('/rest/:_id' , async (res , req ) => {
     } catch (error) {
          return res.status(500).json({error:error.message});
     }
-}):
+}); 
 
 // documentation
 //  Rout   : /rest/:_id
@@ -51,6 +51,33 @@ Router.get('/rest/:_id' , async (res , req ) => {
 //  Access : public
 //  Method : post
 // home work
+
+// documentation
+//  Rout   : /cat/:category
+//  Des    : insert food on the basis of restaurant id
+//  Param  : category 
+//  Access : public
+//  Method : get
+// home work
+
+Router.get('/cat/:category',async (res , req)=>{
+    try {
+           const {category} = req.params;
+           const foods = await FoodModel.find({ 
+            category :{$regex:category , $options: "i" },
+        });
+
+        if(!foods)
+        {
+            return res.status(404).json({error:`no food match with the ${category}`})
+        }
+
+        return res.json({foods})
+
+    } catch (error) {
+        return response.status(500).json({error: error.message});
+    }
+});
 
 
 export default Router;
---
+++ server/api/restaurant/index.js
@@ -0,0 +1,79 @@
+
+import express from 'express'
+
+import {RestaurantModel} from "../../database/allModel"
+
+const Router = express.Router();
+// documentation
+//  Rout   : /
+//  Des    : get all  restaurnat on the basis of city
+//  Param  : none
+//  Access : public
+//  Method : get
+
+Router.get('/', async(req , res)=>{
+  try {
+
+    // we will do it in the form of query 
+    // https://localhost:4000/restaurant/?city = ncr  
+       const {city} = req.query;
+       const restaurants = await RestaurantModel.find({city});
+
+       if(restaurants.length === 0)
+       {
+        return resjson({error:`not found in ${city}`})
+       }
+       return res.json({restaurants});
+  } catch (error) {
+    return res.status(500).json({error: error.message});
+  }
+});
+
+// documentation
+//  Rout   : /:_id
+//  Des    : get restaurant details on the basis of the particular _id
+//  Param  : city
+//  Access : public
+//  Method : get
+
+Router.get('/:_id', async (req , res )=>{
+    
+    const {_id} = req.params;
+    const restaurant = await  RestaurantModel.findById(_id);
+
+    if(!restaurant)
+    {
+        return res.status(404).json({error:"not found"})
+    }
+
+    return res.json({restaurant});
+});
+
+// documentation
+//  Rout   : /search/:searchString
+//  Des    : get restaurant details on the basis of  particular searchString
+//  Param  : searchString
+//  Access : public
+//  Method : get
+
+Router.get('/search/:searchString', async (req , res )=>{
+        
+   try {
+       const {searchString} = req.params;
+       const restaurants = await RestaurantModel.find({
+          name : {$regex : searchString , $options : "i"},
+       });
+
+       if (!restaurants) {
+
+        return res.status(404).json({error : `not found ${searchString}`})
+        
+       }
+
+       return res.json({restaurants});
+    
+   } catch (error) {
+     return res.status(500).json({error:error.message});
+   }
+});
+export default Router;
